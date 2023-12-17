d7533bf578c89c474348b7b5df2ededd22873803
MarceloVilchs <m.vilchess@duocuc.cl>
2023-12-17 11:58:26 -0300

    fin de proyecto

--- src/views/listarCita.vue
+++ src/views/listarCita.vue
@@ -34,6 +34,38 @@
       </div>
     </div>
 
+<!-- Cuadro de diálogo de edición -->
+<div v-if="showDialogEdit" class="dialog-container">
+  <!-- Campos de edición -->
+  <div class="edit-field">
+    <label for="edit-name">Nombre:</label>
+    <input v-model="citaToEdit.name" id="edit-name" class="edit-input" placeholder="Nombre" />
+  </div>
+  <div class="edit-field">
+    <label for="edit-last-name">Apellido:</label>
+    <input v-model="citaToEdit.last_name" id="edit-last-name" class="edit-input" placeholder="Apellido" />
+  </div>
+  <div class="edit-field">
+    <label for="edit-last-name2">Apellido materno:</label>
+    <input v-model="citaToEdit.last_name2" id="edit-last-name2" class="edit-input" placeholder="Apellido materno" />
+  </div>
+  <div class="edit-field">
+    <label for="edit-email">Correo Electrónico:</label>
+    <input v-model="citaToEdit.email" id="edit-email" class="edit-input" placeholder="Correo Electrónico" />
+  </div>
+  <div class="edit-field">
+    <label for="edit-phone">Teléfono:</label>
+    <input v-model="citaToEdit.phone" id="edit-phone" class="edit-input" placeholder="Teléfono" />
+  </div>
+
+  <!-- Botones de acción -->
+  <div class="edit-buttons">
+    <button class="save-btn" @click="editarCita">Guardar</button>
+    <button class="cancel-btn" @click="cerrarDialogoEdicion">Cancelar</button>
+  </div>
+</div>
+
+
     <!-- Cuadro de diálogo de confirmación -->
     <div v-if="showDialog" class="dialog-container">
       <p>
@@ -42,15 +74,6 @@
       <button v-if="citaToDelete" @click="confirmDeleteCita" class="confirm-btn">Sí, eliminar</button>
       <button @click="cancelDeleteCita" class="cancel-btn">Cancelar</button>
 
-
-       <!-- Diálogo de edición -->
-        <EditarCita
-          :dialogoEdicion="showDialogEdit"
-          :citaEditada="citaToEdit"
-          @guardar="editarCita"
-          @cerrar="cancelarEdicionCita"
-        />
-
     </div>
   </div>
 </template>
@@ -58,10 +81,11 @@
 <script lang="ts">
 import axios from "axios";
 import { Vue, Component } from 'vue-property-decorator';
-import EditarCita from "./EditarCita.vue";
 
 @Component({
-  'editar-cita': EditarCita,
+  name: 'ListarCita',
+  components: {
+  },
 })
 
 export default class ListarCita extends Vue {
@@ -147,33 +171,87 @@ export default class ListarCita extends Vue {
   /*---------------------------------------------------*/
 
   /*--------- METODO PUT ------------*/
-  openEditHoraDialog(cita: any) {
+
+  // Método para guardar los cambios en la cita
+  editarCita() {
+  // Validar la cita editada
+  if (!this.validarCita(this.citaToEdit)) {
+    // Muestra un mensaje al usuario indicando que los datos no son válidos
+    console.error('Los datos de la cita no son válidos');
+    return;
+  }
+
+  // Realiza la lógica para actualizar la cita en la base de datos
+  const nombreUsuario = this.citaToEdit.name; // Nombre del usuario que se está editando
+
+  // Construir el objeto de datos a enviar en la solicitud PUT
+  const data = {
+    name: this.citaToEdit.name,
+    last_name: this.citaToEdit.last_name,
+    last_name2: this.citaToEdit.last_name2,
+    email: this.citaToEdit.email,
+    phone: this.citaToEdit.phone,
+    // Agrega otros campos según sea necesario
+  };
+
+  const url = `http://localhost:8000/usuarioFormulario/usuarioFormulario/PUT?nombre=${nombreUsuario}`;
+  const params = { nombre: nombreUsuario };
+  const headers = {
+    Authorization: `Bearer ${this.$store.state.token}`,
+    'Content-Type': 'application/json',
+  };
+
+  axios.put(url, data, { params, headers })
+    .then(response => {
+      console.log('Cita actualizada exitosamente:', response.data.message);
+
+      // Puedes realizar acciones adicionales después de la actualización
+
+      // Después de guardar, cierra el diálogo
+      this.cerrarDialogoEdicion();
+    })
+    .catch(error => {
+      console.error('Error al actualizar la cita:', error);
+
+      // Muestra errores específicos o un mensaje genérico al usuario
+      if (error.response) {
+        console.error('Respuesta del servidor:', error.response.data);
+        console.error('Código de estado HTTP:', error.response.status);
+      } else if (error.request) {
+        console.error('No se recibió respuesta del servidor');
+      } else {
+        console.error('Error durante la solicitud:', error.message);
+      }
+
+      // Si necesitas revertir los cambios en la interfaz, puedes hacerlo aquí
+    });
+}
+
+
+
+validarCita(cita: any): boolean {
+  // Agrega lógica de validación según tus necesidades
+  // Por ejemplo, verifica que los campos obligatorios estén presentes y tengan datos válidos
+
+  if (!cita || !cita.name || !cita.email || !cita.phone) {
+    return false;
+  }
+
+  return true;
+}
+
+openEditHoraDialog(cita: any) {
+  if (cita) {
     this.showDialogEdit = true;
     this.citaToEdit = cita;
   }
+}
 
   // Método para cerrar el diálogo de edición
   cerrarDialogoEdicion() {
     this.showDialogEdit = false;
     this.citaToEdit = null;
-  }
-
-  // Método para guardar los cambios en la cita
-  editarCita(citaEditada: any) {
-    // Aquí deberías realizar la lógica para actualizar la cita en tu base de datos o en la lista de citas
-    // Puedes acceder a la cita original con this.citaToEdit y a la cita editada con citaEditada
-    console.log('Guardando cambios:', citaEditada);
-
-    // Después de guardar, cierra el diálogo
-    this.cerrarDialogoEdicion();
-  }
-
-  // Método para cancelar la edición de la cita
-  cancelarEdicionCita() {
-    // Cierra el diálogo sin guardar cambios
-    this.cerrarDialogoEdicion();
-  }
-  
+  }  
   /*---------------------------------------------------*/
 
   pagarOnline(citaId) {
@@ -292,6 +370,67 @@ export default class ListarCita extends Vue {
   margin: 0 10px;
   background-color: grey;
   color: white;
-  /* Cambia el color del texto */
-}</style>
-  
+  /* Cambia el color del texto */}
+ /* Ajustes de estilo para el cuadro de diálogo de edición */
+.edit-field {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+}
+
+.edit-field label {
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+/* Alinea el texto de los campos a la derecha */
+.edit-field input {
+  text-align: right;
+}
+
+/* Estilo específico para los campos de edición */
+.edit-field input {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 8px;
+  margin-top: 5px;
+  font-size: 14px;
+}
+
+/* Alinea los botones al final con espacio entre ellos */
+.edit-buttons {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 15px;
+}
+
+/* Estilo para el botón Guardar */
+.save-btn {
+  background-color: #3498db; /* Azul */
+  color: white;
+  border: none;
+  padding: 10px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.save-btn:hover {
+  background-color: #2980b9; /* Cambio de color al pasar el ratón */
+}
+
+/* Estilo para el botón Cancelar */
+.cancel-btn {
+  background-color: #95a5a6; /* Gris */
+  color: white;
+  border: none;
+  padding: 10px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.cancel-btn:hover {
+  background-color: #7f8c8d; /* Cambio de color al pasar el ratón */
+}
+</style>
